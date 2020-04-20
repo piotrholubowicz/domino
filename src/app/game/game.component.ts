e076e46318db2d5d175497ae1f23a948c5bfcce5
Piotr Hołubowicz <piotrekhol@gmail.com>
2020-04-19 23:07:27 -0700

    Load the game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Observable, EMPTY, of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, switchMap } from 'rxjs/operators';
 
 import { GameService } from '../game.service';
+import { Game } from '../game';
 
 @Component({
   selector: 'app-game',
@@ -12,9 +13,33 @@ import { GameService } from '../game.service';
   styleUrls: ['./game.component.css'],
 })
 export class GameComponent implements OnInit {
-  constructor(private service: GameService, private router: Router) {}
+  game$: Observable<Game>;
 
-  ngOnInit(): void {}
+  constructor(
+    private service: GameService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.game$ = this.route.paramMap.pipe(
+      switchMap((_) => {
+        return this.service.getGamePolling().pipe(
+          tap((game) => {
+            if (game.state === 'NO_GAME') {
+              this.router.navigate(['team']);
+            }
+          })
+        );
+      })
+    );
+  }
+
+  onEndGame(event: any, id: string) {
+    this.service.deleteGame(id).subscribe((_) => {
+      this.router.navigate(['team']);
+    });
+  }
 
   player(): string {
     return this.service.getPlayer() || 'Guest';
