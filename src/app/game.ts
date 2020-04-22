21dfdf4e5cb11df7d7a7ac9064fe3dac6eb49c68
Piotr Hołubowicz <piotrekhol@gmail.com>
2020-04-21 22:38:10 -0700

    shows last move and score

--- src/app/game.ts
+++ src/app/game.ts
@@ -6,5 +6,6 @@ export interface Game {
   table: number[][]; // list of pieces from left to right
   firstPiece: number[]; // the first piece that was played, for table positioning
   currentPlayer: string;
+  lastMove: any;
   scoreLog: number[][];
 }
