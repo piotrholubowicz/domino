d74ae136a5781eb5984f249c09839eb17cd9c843
Piotr Hołubowicz <piotrekhol@gmail.com>
2020-05-09 18:57:50 -0700

    start a notification service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  readonly VAPID_PUBLIC_KEY =
+    'BBv10bLqL5Mhkuggm3rhWfo2gfKCOUBDdmyOAt7o_0TegWajIhXHzFlMND8YRg2vq1qpSOuUB1aJTWLGos-HCHM';
   title = 'domino';
 }
